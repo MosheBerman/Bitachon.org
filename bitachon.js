e26f10cca95efbbc4f2e45f65b58bfbe8410f00c
Moshe <moshberm@gmail.com>
2011-01-13 16:31:19 -0500

    implemented form in header

--- bitachon.js
+++ bitachon.js
@@ -9,6 +9,9 @@ $(document).ready(function(){
     var expandedContactInfoHeight = $('#footer #contactInfo').outerHeight(true);
     var expandedAboutSiteHeight = $('#footer #aboutSite').outerHeight(true);
     
+    var collapsedWidth = 60;
+    var fullWidthOfWindow = $(document).width();
+    var formObj = "";
     
     $('#footer').css({
         'margin-top': -collapsedHeight,
@@ -17,39 +20,104 @@ $(document).ready(function(){
     
     $('#main').css({ 'padding-bottom': collapsedHeight });
     
-    /* Show the Bitachon Builder Request Section */
+    /* ----------------- Show the Bitachon Builder Request Section --------------------------- */
     
-    $('#showBBRequest').click(function(evt) {
-        $('#footer .collapsed-view').fadeOut();
-        $('#main').animate({
-            'padding-bottom': expandedBBRequestHeight
-        });
-        $('#footer').animate({
-            'margin-top': -expandedBBRequestHeight,
-            'height': expandedBBRequestHeight
+    /*
+    $('#showBBRequestButton').click(function(evt) {
+        $('#showBBRequestButton').fadeOut();
+        $('#requestBitachonBuilderForm').animate({
+            'width': 100
         }, null, null, function() {
-            $('#footer #requestBitachonBuilder').fadeIn();
+            $('#BBRequestBox').fadeIn();
         });
         return false;
     });
     
-    /* Hide the BB Request Section */
-    $('#hideBBRequest').click(function(evt) {
-        $('#footer #requestBitachonBuilder').fadeOut();
-        
-        $('#main').animate({
-            'padding-bottom': collapsedHeight
-        });
-        
-        $('#footer').animate({
-            'margin-top': -collapsedHeight,
-            'height':collapsedHeight
-        }, null, null, function() {
-            $('#footer .collapsed-view').fadeIn();
-        });
+    /// Hide the BB Request Section /
+    $('#hideBBRequestButton').click(function(evt) {
+        $('BBRequestBox').fadeOut();
+         $('#showBBRequestButton').fadeIn();
+        $('#requestBitachonBuilderForm').animate({
+            'width': collapsedWidth
+        }, null, null, null);
         return false;
     });
-
+    
+    */
+    
+   
+    
+    $('#showRequestFormButton').click(
+    	function(evt){
+    		
+    		//If the form has been saved, reload it now.
+    		if(formObj){
+    			$('#requestForm').html(formObj);
+    		}
+    		
+    		//Hide the "show" button
+    		$('#showRequestFormButton').fadeOut();
+    		
+    		//	
+    		//	In case the user resizes their window
+    		//	while using the site, reassign this value
+    		//
+    		
+    		if(fullWidthOfWindow){
+    			fullWidthOfWindow = $(document).width();
+    		}
+    		
+    		//animate the flyout
+    		$('#flyout').animate({
+    			'width': fullWidthOfWindow
+    		}, null, null, function(){
+    			$('#requestForm').fadeIn();
+    	
+    			//fade the hide button in
+   		 		$('#hideRequestFormButton').fadeIn();
+    		});
+    		
+    		$('#requestFormContainer').fadeIn();
+    		
+    		return false;
+    		
+    	}
+    	
+    );
+    
+    
+		//Reverse everything
+		$('#hideRequestFormButton').click(
+    	function(evt){
+    		
+    		//Hide the "show" button
+    		$('#requestForm').fadeOut();
+    		$('#requestFormContainer').fadeOut(
+    			function(evt){
+    				//animate flyout width to the narrower state
+    				$('#flyout').animate({
+    					'width': 160
+    				});
+    			}
+    		
+    		);
+    		
+    		
+    		//Hide the form
+    		$('#hideRequestFormButton').fadeOut();
+    		
+    		//fade the hide button in
+    		$('#showRequestFormButton').fadeIn();
+    		
+    		//TODO: Fix this bug which prevents the form from properly re-appearing for some reason.
+			//$('#requestFormContainer').html(''+formHTML+'');
+    		    		
+    		return false;
+    	}
+    );
+    
+    /* ----------------- END Show the Bitachon Builder Request Section --------------------------- */
+    
 	/* show the contact info footer section */
     $('#showContactInfo').click(function(evt) {
         $('#footer .collapsed-view').fadeOut();
@@ -109,22 +177,27 @@ $(document).ready(function(){
     
     /* Submit the bitachon builder request via AJAX*/
     
-    $('#submitButton').click(function(){
+    $('#submitButton').click(function(evt){
     
     	//Store the contents of the form
     	var addressArray = new Array($('#name').val(), $('#street').val(), $('#city').val(), $('#state').val(), $('#zip').val());
     		
-    	$('#BBRequestBox').html('<img src="images/loading.gif" />');
+    	formObj = $('#requestForm').html();
+    	
+    	$('#requestForm').html('<img src="images/loading.gif" />');
   		
   		$.ajax({
 				type: "POST",
    				url: "./bbrequest.php",
 	   			data: {name: addressArray[0], street: addressArray[1], city: addressArray[2], state: addressArray[3], zip: addressArray[4] },
    				success: function(msg){
-    				$('#BBRequestBox').html('<p>' + msg + '</p>');
+    				$('#requestForm').html('<p class="formResponse">' + msg + '</p>');
+    				return false;
 				},
 				error: function(XMLHttpRequest, textStatus, errorThrown){
-					$('#BBRequestBox').html('<p> There\'s been an error: ' + errorThrown + '</p>');
+					$('#requestForm').html('<p class="formResponse"> There\'s been an error: ' + errorThrown + '</p>');
+					
+					return false;
 				}
 		});
   	return false;
--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
 /* ---------------------------------------------
-/	File: template.php
+/	File: index.php
 /
 /
 /	Created By: Moshe Berman
 /
 /	On: October 14, 2010
 /
-/	Last Edited on: December 5, 2010
+/	Last Edited on: January 12, 2010
 /
 /	Contains: The template for Bitachon.org
 /
@@ -41,9 +41,26 @@
 	<body>
 		<div id="wrap">
 			<div id="main">
+						
 				<div id="topbar" class="dark-bg">
 					<div id="banner">
 					</div>
+					<div id="flyout">
+						<a id="showRequestFormButton" href="#">Show</a>						
+						<div id="requestFormContainer" style="display: none;">	
+							<div id="requestFormWindow">
+								<a id="hideRequestFormButton" style="display: none;" href="#">Hide</a>
+								<form id="requestForm" action="#" style="display:none;">
+									<label>Name:</label><input type="text" name="name" id="name" class="textbox" />
+									<label>Street:</label><input type="text" name="street" id="street" class="textbox" />
+									<label>City:</label><input type="text" id="city" class="textbox" />
+									<label>State:</label><input type="text" id="state" class="textbox" />
+									<label>Zip:</label><input type="text" id="zip" class="textbox" />
+									<input type="submit" value="Send Me a Bitachon Builder!" id="submitButton" />
+								</form>
+							</div>
+						</div>
+					</div>					
 					<div id="nav">
 						<a href="http://bitachon.org" class="link">
 							Search
@@ -53,35 +70,17 @@
 						</a>
 						<a href="http://bitachon.org" class="link">
 							Donate
-						</a>								
+						</a>	
 					</div>
 				</div>
 			</div>
 		</div>
 		<div id="footer" class="dark-bg">
 		  <div class="collapsed-view center full-width">
-		  	<a href="" id="showBBRequest" class="toggle-link">Get a Bitachon Builder</a> <a href="" id="showContactInfo" class="toggle-link">Contact Priority-1</a> <a href="" id="showAboutSite" class="toggle-link">About Bitachon.org</a>
+		  	<a href="" id="showContactInfo" class="toggle-link">Contact Priority-1</a> <a href="" id="showAboutSite" class="toggle-link">About Bitachon.org</a>
 		  	<div>
 		  	</div>
 		  </div>
-		  <div id="requestBitachonBuilder" class="expanded-view full-width">
-		  	  <div class="center">
-			 	 <div class="footer-expanded-column narrow border">
-			  		<h4 class="title">Please enter your address:</h4>
-			  		<div id="BBRequestBox">
-					<form action="#">
-						<label>Name:</label><input type="text" name="name" id="name" class="textbox" />
-						<label>Street:</label><input type="text" name="street" id="street" class="textbox" />
-						<label>City:</label><input type="text" id="city" class="textbox" />
-						<label>State:</label><input type="text" id="state" class="textbox" />
-						<label>Zip:</label><input type="text" id="zip" class="textbox" />
-						<input type="submit" value="Send Me a Bitachon Builder!" id="submitButton" />
-					</form>
-					</div>
-				</div>
-		  </div>	
-		  <a href="" id="hideBBRequest" class="toggle-link close-link shiny-link"><img src="./images/downarrow.png" class="arrow"/> Close</a>
-		</div>
 		  <div id="contactInfo" class="expanded-view full-width">
 			<div class="center full-width">
 				<div class="footer-expanded-column">
@@ -101,7 +100,7 @@
 			<div class="center full-width">
 				<div class="footer-expanded-column">
 					<img src="images/logo-sm.png" class="footer-logo" /><br />
-					<h4 id="site-info">Bitachon.org version 0.1 by <a href="http://www.mosheberman.com" class="footer-link shiny-link">Moshe Berman</a></h4>
+					<h4 id="site-info">Bitachon.org version 0.2 by <a href="http://www.mosheberman.com" class="footer-link shiny-link">Moshe Berman</a></h4>
 				</div>
 			</div>
 			<a href="" id="hideAboutSite" class="toggle-link close-link shiny-link"><img src="./images/downarrow.png" class="arrow"/> Close</a>
